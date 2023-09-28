bf1aaaf469fbd45fb398827099aa0822b84df80d
DevRafaelDeMelo <rafaelmelo765@gmail.com>
2023-09-27 22:59:55 -0300

    V 27.09.2023

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+app.get("/teste", (req, res) =>
+  res.status(200).send({
+    mensagem: "Boas_Vindas a Apisssss",
+  })
+);
+
+app.listen(port, () => console.log("servidor está rodando !!!"));
+
+module.exports = app;
